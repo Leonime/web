e3203cc9770587eb417e402a8eefcff2dc0640be
Leopoldo Parra <lparra.dev@gmail.com>
2019-02-01 16:48:51 -0800

    added utilities to create the short code

---
+++ shortener/utils.py
@@ -0,0 +1,19 @@
+import random
+import string
+from django.conf import settings
+
+SHORT_CODE_MIN = getattr(settings, "SHORT_CODE_MIN", 6)
+
+
+def code_generator(size=SHORT_CODE_MIN, char=string.ascii_letters + string.digits):
+    return ''.join(random.SystemRandom().choice(char) for _ in range(size))
+    pass
+
+
+def create_short_code(instance, size=SHORT_CODE_MIN):
+    new_code = code_generator(size=size)
+    obj = instance.__class__
+    qs_exists = obj.objects.filter(short_code=new_code).exists()
+    if qs_exists:
+        return create_short_code(instance, size=size)
+    return new_code
