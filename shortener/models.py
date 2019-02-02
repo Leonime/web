312f5f6dc1836a44584f24c58ed4fd80e8411109
Leopoldo Parra <lparra.dev@gmail.com>
2019-02-01 16:55:08 -0800

    created the modal for shortener app

--- shortener/models.py
+++ shortener/models.py
@@ -1,3 +1,48 @@
+from django.conf import settings
+from django.contrib.sites.models import Site
 from django.db import models
+from django.urls import reverse
+
+from shortener.validators import validate_url
+from .utils import create_short_code
+
+SHORT_CODE_MAX = getattr(settings, "SHORT_CODE_MAX", 16)
+
+
+class ShortURLManager(models.Manager):
+    @staticmethod
+    def refresh_short_codes():
+        qs = ShortURL.objects.filter(id__gte=1)
+        new_codes = 0
+        for q in qs:
+            q.short_code = create_short_code(q)
+            q.save()
+            new_codes += 1
+        return f"New codes made: {new_codes}"
+
 
 # Create your models here.
+class ShortURL(models.Model):
+    url = models.CharField(max_length=226, validators=[validate_url])
+    short_code = models.CharField(max_length=SHORT_CODE_MAX, unique=True)
+    updated = models.DateTimeField(auto_now=True)  # every time the model is saved
+    timestamp = models.DateTimeField(auto_now_add=True)  # when model was created
+    active = models.BooleanField(default=True)
+
+    objects = ShortURLManager()
+
+    def save(self, *args, **kwargs):
+        if not self.short_code:
+            self.short_code = create_short_code(self)
+        super(ShortURL, self).save(*args, **kwargs)
+        pass
+
+    def get_absolute_url(self):
+        domain = Site.objects.get_current().domain
+        return f"http://{domain}{reverse('short_code', kwargs={'short_code': self.short_code})}"
+
+    def __str__(self):
+        return f'{self.url + self.short_code}'
+
+    def __unicode__(self):
+        return f'{self.url + self.short_code}'
