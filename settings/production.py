3c8b60badf2be25eafa945a4b13b9e4275cf80b1
Leopoldo Parra <lparra.dev@gmail.com>
2019-02-04 03:26:38 -0800

    Refactored how decryption was being handled removed a lot of unnecessary code.

--- settings/production.py
+++ settings/production.py
@@ -1,12 +1,4 @@
-import base64
-import json
-
-from cryptography.fernet import Fernet
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-
-from core.utils import decrypt
+from core.utils import load_db_config
 from settings.base import *
 
 DEBUG = False
@@ -14,20 +6,7 @@ TEMPLATES[0]['OPTIONS']['debug'] = DEBUG
 
 ALLOWED_HOSTS = ['.codeshepherds.com']
 
-with open(os.path.join(BASE_DIR, "config.json"), 'r') as cfg:
-    config = json.load(cfg)
-
-password = str.encode(CRYPT_KEY)
-salt = b'd\x04\xe7@T\xd6\x8e\xac\xa5\xd9\xfb\x17o\xc0\xc2g'
-kdf = PBKDF2HMAC(
-    algorithm=hashes.SHA256(),
-    length=32,
-    salt=salt,
-    iterations=100000,
-    backend=default_backend()
-)
-key = base64.urlsafe_b64encode(kdf.derive(password))
-f = Fernet(key)
+config = load_db_config('production.json')
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
@@ -35,10 +14,10 @@ f = Fernet(key)
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': f.decrypt(str.encode(config["DB"]["NAME"]).decode("utf-8")),
-        'USER': f.decrypt(str.encode(config["DB"]["USER"]).decode("utf-8")),
-        'PASSWORD': f.decrypt(str.encode(config["DB"]["PASSWORD"]).decode("utf-8")),
-        'HOST': f.decrypt(str.encode(config["DB"]["HOST"]).decode("utf-8")),
-        'PORT': '',
+        'NAME': config["DB"]["NAME"],
+        'USER': config["DB"]["USER"],
+        'PASSWORD': config["DB"]["PASSWORD"],
+        'HOST': config["DB"]["HOST"],
+        'PORT': config["DB"]["PORT"],
     }
 }
