cec3687bf2a221261e5cc9e26dcf574ff2bc514b
Leopoldo Parra <lparra.dev@gmail.com>
2019-02-07 22:08:42 -0800

    updated design

--- assets/js/shortener/success.js
+++ assets/js/shortener/success.js
@@ -1,10 +1,12 @@
-$('.short-url').click(function () {
+$('span.short-url').click(function () {
     $(this).select();
 });
 
 $('button.btn-copy').click(function () {
     var $temp = $("<input>");
-    var $text = $('.short-url').text();
+    var short_url = $('span.short-url');
+    short_url.select();
+    var $text = short_url.text();
     $("body").append($temp);
     $temp.val($text).select();
     document.execCommand("copy");
