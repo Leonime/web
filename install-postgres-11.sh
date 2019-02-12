bf246ae2258cf56f20154e981e0da6e8cd474573
Leopoldo Parra <lparra.dev@gmail.com>
2019-02-12 14:27:04 -0800

    Not working returning to previous configuration.

--- install-postgres-11.sh
+++ install-postgres-11.sh
@@ -4,13 +4,13 @@ set -ex
 
 echo "Installing Postgres 10"
 sudo service postgresql stop
-sudo apt-get remove -q 'postgresql-*'
+sudo apt-get remove -qq 'postgresql-*'
 
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 RELEASE=$(lsb_release -cs)
 echo "deb http://apt.postgresql.org/pub/repos/apt/ ${RELEASE}"-pgdg main | sudo tee  /etc/apt/sources.list.d/pgdg.list
-sudo apt update -q
-sudo apt install -q postgresql-11 postgresql-client-11
+sudo apt update -qq
+sudo apt install -qq postgresql-11 postgresql-client-11
 sudo cp /etc/postgresql/{9.6,11}/main/pg_hba.conf
 
 echo "Restarting Postgres 10"
