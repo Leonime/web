ae5d889905417d7e04576cdbd55f35ac52f639d1
Leopoldo Parra <lparra.dev@gmail.com>
2019-02-14 22:22:08 -0800

    Configured correctly

--- codeshepherds/wsgi.py
+++ codeshepherds/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "codeshepherds.settings")
+settings = os.environ.get('FILE_SETTINGS')
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings." + settings)
 
 application = get_wsgi_application()
