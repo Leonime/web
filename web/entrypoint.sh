d23f23741164ec6aafc97712a05959e9534935ba
Leopoldo Parra <lparra.dev@gmail.com>
2019-11-18 03:51:58 -0800

    Updated entrypoint

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -2,13 +2,11 @@
 
 if [ "$DATABASE" = "postgres" ]
 then
-    echo "Waiting for PostgreSQL..."
-
-    while ! nc -z $SQL_HOST $SQL_PORT; do
-      sleep 0.1
-    done
-
-    echo "PostgreSQL started"
+  printf "\n\033[1mWaiting for postgres...\033[0m\n"
+  while ! nc -z "$SQL_HOST" "$SQL_PORT"; do
+    sleep 0.1
+  done
+  printf "PostgreSQL started\n\n"
 fi
 
 python manage.py flush --no-input
