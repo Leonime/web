7e43b64f74aa6731fbd43d93836ac535598c7a2f
Leopoldo Parra <lparra.dev@gmail.com>
2019-11-28 16:23:08 -0800

    Initial app
    For the moment it does nothing.

---
+++ configurator/configurator/app.py
@@ -0,0 +1,31 @@
+import argparse
+
+from configurator.corelibs.argumentator import ArgumentHandler
+from configurator.corelibs.secrets import Secrets
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    argumentator = ArgumentHandler(parser=parser)
+    parser = argumentator.args_builder()
+    args = parser.parse_args()
+
+    interactive = bool(args.interactive)
+    verbosity = bool(args.verbosity)
+
+    # password = get_input(confirmation=True, msg='Are you sure? ')
+
+    secret = Secrets(interactive, verbosity)
+    secrets_dir = secret.set_secrets_folder(args)
+
+    if args.default and args.interactive:
+        print('You can\'t use -i and -d at the same time.')
+    elif args.default:
+        print(f'Files will be stored in: {secrets_dir}')
+        if not secrets_dir.exists():
+            secrets_dir.mkdir()
+            pass
+
+
+if __name__ == '__main__':
+    main()
