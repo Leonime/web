fae21bc17a49256cefd0661c8e1a145ed1d506c4
Leopoldo Parra <lparra.dev@gmail.com>
2019-11-28 16:16:46 -0800

    Added a function
    An all purpose terminal input capture function.

---
+++ configurator/configurator/corelibs/utils.py
@@ -0,0 +1,31 @@
+from getpass import getpass
+
+from colorama import Fore
+
+
+def get_input(blank=False, is_pass=False, confirmation=False, msg=''):
+    value = None
+    if confirmation:
+        value = input(f'{Fore.GREEN + msg}[y/N]: {Fore.RESET}')
+        if (value == 'y' or value == 'Y') or value.strip() == '':
+            return True
+        elif value == 'N':
+            return False
+        else:
+            raise Exception('Invalid input')
+    else:
+        while value is None:
+            if not is_pass:
+                value = input(f'{Fore.GREEN}{msg}{Fore.RESET}')
+                pass
+            else:
+                value = getpass(f'{Fore.GREEN}{msg}')
+                confirmation = getpass(f'{Fore.GREEN}Confirm password: ')
+                if value != confirmation:
+                    print(f'{Fore.RED}Passwords don\'t match{Fore.RESET}')
+                    value = None
+                    continue
+            if not value and not blank:
+                print(f'{Fore.RED}Input can\'t be blank{Fore.RESET}')
+                value = None
+    return value
