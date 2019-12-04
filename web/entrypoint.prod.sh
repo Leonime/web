1119588c5015c7c67bb61733eac4259f957c9ee5
Leopoldo Parra <lparra.dev@gmail.com>
2019-12-04 00:38:35 -0800

    Added script that expands the variables from docker secrets

--- web/entrypoint.prod.sh
+++ web/entrypoint.prod.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+. /home/app/web/env_secrets_expand.sh
+env_secrets_expand
+
 if [ "$DATABASE" = "postgres" ]
 then
   printf "\n\033[1mWaiting for postgres...\033[0m\n"
-  while ! nc -z "$SQL_HOST" "$SQL_PORT"; do
+  while ! nc -z "$SQL_HOST_TEST" "$SQL_PORT_TEST"; do
     sleep 0.1
   done
   printf "PostgreSQL started\n\n"
--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
+. /usr/src/app/env_secrets_expand.sh
+env_secrets_expand
+
 if [ "$DATABASE" = "postgres" ]
 then
   printf "\n\033[1mWaiting for postgres...\033[0m\n"
-  while ! nc -z "$SQL_HOST" "$SQL_PORT"; do
+  while ! nc -z "$SQL_HOST_TEST" "$SQL_PORT_TEST"; do
     sleep 0.1
   done
   printf "PostgreSQL started\n\n"
 fi
 
+python manage.py collectstatic --no-input --clear
 python manage.py flush --no-input
 python manage.py migrate
-python manage.py collectstatic --no-input --clear
 
 exec "$@"
