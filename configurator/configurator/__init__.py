dd6261387b3a0a62028d272880cad42a73257d55
Leopoldo Parra <lparra.dev@gmail.com>
2019-12-03 17:51:49 -0800

    fixed path.

--- configurator/configurator/__init__.py
+++ configurator/configurator/__init__.py
@@ -3,5 +3,5 @@ from pathlib import Path
 version_info = (0, 0, 1)
 version = '.'.join(str(c) for c in version_info)
 
-BASE_DIR = Path.cwd().parent
+BASE_DIR = Path.cwd().parent.parent
 ENV_SECRETS_DIR = 'secrets'
