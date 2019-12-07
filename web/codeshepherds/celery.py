a87a0ea7b69acc69320af6bafebf468ca9cbf0f6
Leopoldo Parra <lparra.dev@gmail.com>
2019-12-07 15:09:26 -0800

    configured celery

--- web/codeshepherds/celery.py
+++ web/codeshepherds/celery.py
@@ -7,3 +7,8 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.settings')
 celery_app = Celery('codeshepherds')
 celery_app.config_from_object('django.conf:settings', namespace='CELERY')
 celery_app.autodiscover_tasks()
+
+
+@celery_app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
