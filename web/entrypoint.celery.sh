4f38708f321637b154769b1525630ebb343de13c
Leopoldo Parra <lparra.dev@gmail.com>
2019-12-07 16:28:22 -0800

    Added celery to docker-compose

---
+++ web/entrypoint.celery.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+. /usr/src/app/env_secrets_expand.sh
+env_secrets_expand
+
+if [ "$DATABASE" = "postgres" ]
+then
+  printf "\n\033[1mWaiting for postgres...\033[0m\n"
+  while ! nc -z "$SQL_HOST_TEST" "$SQL_PORT_TEST"; do
+    sleep 0.1
+  done
+  printf "PostgreSQL started\n\n"
+fi
+
+exec "$@"
