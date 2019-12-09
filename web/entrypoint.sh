0d84f94e4bf83b1cbba8babbb9d6fa7172bbd225
Leopoldo Parra <lparra.dev@gmail.com>
2019-12-08 20:29:30 -0800

    Unified entrypoint
    Now instead of having multiple entrypoints all is handled by a single file.

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -11,11 +11,24 @@ if [ "$DATABASE" = "postgres" ]; then
   printf "PostgreSQL started\n\n"
 fi
 
-python manage.py collectstatic --no-input --clear
-python manage.py flush --no-input
-python manage.py makemigrations
-python manage.py migrate
-python manage.py loaddata ./cookbook/fixtures/initial_data.json
-python manage.py loaddata ./base/fixtures/initial_data.json
+if [ "$CONTAINER" = "development" ]; then
+  printf "#####\n"
+  printf "# Django development setup\n"
+  printf "#####\n"
+  python manage.py collectstatic --no-input --clear
+  python manage.py flush --no-input
+  python manage.py makemigrations
+  python manage.py migrate
+  python manage.py loaddata ./cookbook/fixtures/initial_data.json
+  python manage.py loaddata ./base/fixtures/initial_data.json
+elif [ "$CONTAINER" = "production" ]; then
+  printf "#####\n"
+  printf "# Django production setup\n"
+  printf "#####\n"
+  python manage.py collectstatic --no-input --clear
+  python manage.py migrate
+  python manage.py loaddata ./cookbook/fixtures/initial_data.json
+  python manage.py loaddata ./base/fixtures/initial_data.json
+fi
 
 exec "$@"
