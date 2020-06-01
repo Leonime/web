7a4bd0f166fe89ad7b9e30328b23ab1a81b76a60
Leopoldo Parra <lparra.dev@gmail.com>
2020-05-31 17:47:27 -0700

    Created some base models.

--- web/forum/models.py
+++ web/forum/models.py
@@ -1,3 +1,17 @@
+from django.conf import settings
 from django.db import models
 
+
 # Create your models here.
+class Forum(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    forum = models.ForeignKey(Forum, on_delete=models.CASCADE)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
