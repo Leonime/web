d49ae67320bcf16d7bbc8faa64ae7c3f7c64b3f4
Leopoldo Parra <lparra.dev@gmail.com>
2020-06-01 02:00:50 -0700

    Created basic view for signup

--- web/account/views.py
+++ web/account/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
-from django.views import View
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 
-class SignUp(View):
-    def post(self, request, *args, **kwargs):
-        pass
+class SignUp(CreateView):
+    model = User
+    success_url = reverse_lazy('login')
+    template_name = 'account/signup.html'
+    fields = ['username', 'first_name', 'last_name', 'email', 'password']
