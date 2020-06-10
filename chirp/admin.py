5c309eb6849618cd52977596eb4a43ce1a2efa75
Leopoldo Parra <lparra.dev@gmail.com>
2020-06-10 09:17:56 -0700

    Started implementation of likes

--- chirp/admin.py
+++ chirp/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 
-from chirp.models import Chirp
+from chirp.models import Chirp, ChirpLike
+
+
+class ChirpLikeAdmin(admin.TabularInline):
+    model = ChirpLike
 
 
 class ChirpAdmin(admin.ModelAdmin):
+    inlines = [ChirpLikeAdmin]
     list_display = ['user', '__str__']
-    search_fields = ['user__username', 'user__email']
+    search_fields = ['content', 'user__username', 'user__email']
 
     class Meta:
         model = Chirp
--- chirp/models.py
+++ chirp/models.py
@@ -2,12 +2,21 @@ import random
 
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
+
+
+class ChirpLike(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tweet = models.ForeignKey("Chirp", on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
 
 class Chirp(models.Model):
     user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    likes = models.ManyToManyField(User, related_name='chirp_user', blank=True, through=ChirpLike)
     content = models.TextField(null=False, blank=False)
     image = models.FileField(null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ['-id']
