0f5c1e6a2edc0bd47444c066d5ea74a1cd790af8
Leopoldo Parra <lparra.dev@gmail.com>
2020-06-10 01:07:53 -0700

    Added user to chirp

--- chirp/models.py
+++ chirp/models.py
@@ -1,14 +1,19 @@
 import random
 
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class Chirp(models.Model):
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     content = models.TextField(null=False, blank=False)
-    image = models.FileField()
+    image = models.FileField(null=True, blank=True)
 
     class Meta:
         ordering = ['-id']
+
+    def __str__(self):
+        return f'{self.content}'
 
     def serialize(self):
         return {
