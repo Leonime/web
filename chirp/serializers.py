44f5bd90794deae112f2512076c53d7e318bbf8d
Leopoldo Parra <lparra.dev@gmail.com>
2020-06-10 23:57:28 -0700

    Updated serializers.py a little

--- chirp/serializers.py
+++ chirp/serializers.py
@@ -1,9 +1,33 @@
-from django.conf import settings
+from abc import ABC
+
 from rest_framework import serializers
+
 from chirp.models import Chirp
+from codeshepherds.settings import CHIRP_ACTION_OPTIONS, MAX_CHIRP_LENGTH
+
+
+class ChirpActionSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    action = serializers.CharField()
+
+    def validate_action(self, value):
+        value = value.lower().strip()
+        if value not in CHIRP_ACTION_OPTIONS:
+            raise serializers.ValidationError("This is not a valid action for tweets")
+        return value
 
 
 class ChirpSerializer(serializers.ModelSerializer):
+    likes = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Chirp
-        fields = ['id', 'content']
+        fields = ['id', 'content', 'likes']
+
+    def get_likes(self, obj):
+        return obj.likes.count()
+
+    def validate_content(self, value):
+        if len(value) > MAX_CHIRP_LENGTH:
+            raise serializers.ValidationError("This tweet is too long")
+        return value
