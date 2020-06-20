bd26c75b0b3637906fe967819df21f570864321a
Leopoldo Parra <lparra.dev@gmail.com>
2020-06-20 01:29:25 -0700

    Need to add some data, but for the moment I'm using the default code.

--- chirp/views.py
+++ chirp/views.py
@@ -53,6 +53,17 @@ class ChirpViewSet(viewsets.ModelViewSet):
     serializer_class = ChirpSerializer
     permission_classes = [IsAuthenticatedOrReadOnly, ]
 
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
     @action(detail=True, methods=['post'])
     def like_action(self, request, pk=None):
         try:
