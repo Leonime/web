6bac55b0346bfb9e87712b4182ab7812ec27659a
Leopoldo Parra <lparra.dev@gmail.com>
2020-06-21 04:18:37 -0700

    Added url

--- frontend/urls.py
+++ frontend/urls.py
@@ -5,4 +5,5 @@ from frontend.views import Index
 app_name = 'frontend'
 urlpatterns = [
     path('frontend/', Index.as_view(), name='index'),
+    path('frontend/<int:chirp_id>/', Index.as_view(), name='index'),
 ]
