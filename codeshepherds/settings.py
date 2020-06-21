b6459be40bad4ca54ca6cd567ac6af723166aca1
Leopoldo Parra <lparra.dev@gmail.com>
2020-06-21 12:49:52 -0700

    Fixed url was not working

--- codeshepherds/settings.py
+++ codeshepherds/settings.py
@@ -56,7 +56,7 @@ REDIS_URL = os.environ.get('REDIS_URL')
 # Auth urls defaults
 LOGIN_URL = 'login/'
 LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = 'login/'
+LOGOUT_REDIRECT_URL = '/login/'
 
 # Installed apps
 PRIORITY_APPS = [
