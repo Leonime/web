d180fa55924cbddcc3fc80971a4c00de65c8a05f
Leopoldo Parra <lparra.dev@gmail.com>
2020-06-21 02:51:52 -0700

    Make it a little more dinamic just testing some things

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {ChirpDetailComponent, ChirpsComponent} from "./chirps";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const appEl = document.getElementById('root')
+if (appEl) {
+    ReactDOM.render(<App/>, appEl);
+}
+const e = React.createElement
+const chirpsEl = document.getElementById("chirp")
+if (chirpsEl) {
+    ReactDOM.render(
+        e(ChirpsComponent, chirpsEl.dataset), chirpsEl);
+}
+
+const chirpDetailElements = document.querySelectorAll(".chirp-detail")
+
+chirpDetailElements.forEach(container => {
+    ReactDOM.render(
+        e(ChirpDetailComponent, container.dataset),
+        container);
+})
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
