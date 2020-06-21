7d004cd7319418b9909a6d065b01190616c66d84
Leopoldo Parra <lparra.dev@gmail.com>
2020-06-21 02:49:04 -0700

    Added some lookups and rearanged the functions a little

--- frontend/src/chirps/lookup.js
+++ frontend/src/chirps/lookup.js
@@ -1,11 +1,19 @@
 import {backendLookup} from '../lookup'
 
-export function apiChirpCreate(newTweet, callback) {
-    backendLookup("POST", "chirps/", callback, {content: newTweet})
+export function apiChirpCreate(newChirp, callback) {
+    backendLookup("POST", "chirps/", callback, {content: newChirp})
 }
 
-export function apiChirpList(callback) {
-    backendLookup("GET", "chirps/", callback)
+export function apiChirpList(username, callback) {
+    let endpoint = "chirps/"
+    if (username) {
+        endpoint = `chirps/?username=${username}`
+    }
+    backendLookup("GET", endpoint, callback)
+}
+
+export function apiChirpDetail(chirpId, callback) {
+    backendLookup("GET", `chirps/${chirpId}/`, callback)
 }
 
 export function apiChirpAction(chirp_id, action, callback) {
