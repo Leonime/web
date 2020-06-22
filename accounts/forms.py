27ecb94173e4cbdf6f7608b6697bf93fb6afc4de
Leopoldo Parra <lparra.dev@gmail.com>
2020-06-21 18:30:48 -0700

    Corrected email validation

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UsernameField
 from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
@@ -6,15 +6,25 @@ from django.core.exceptions import ValidationError
 
 class UserCreateForm(UserCreationForm):
     email = forms.EmailField(required=True, label='eMail')
+    error_messages = {
+        'email_exists': 'The email already exists.'
+    }
 
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
+        field_classes = {
+            'email': forms.EmailField
+        }
 
     def clean_email(self):
+        email = self.cleaned_data.get('email')
         if User.objects.filter(email=self.cleaned_data['email']).exists():
-            raise ValidationError(self.fields['email'].error_messages['exists'])
-        return self.cleaned_data['email']
+            raise ValidationError(
+                self.error_messages['email_exists'],
+                code='email_exists',
+            )
+        return email
 
     def save(self, commit=True):
         user = super(UserCreateForm, self).save(commit=False)
