ca53f0de7ce8d13fcab292ac960677036d26a6a9
Leopoldo Parra <lparra.dev@gmail.com>
2020-06-21 18:31:33 -0700

    Added url for user registration.

--- accounts/url.py
+++ accounts/url.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from accounts.views import UserLoginView, UserLogoutView
+from accounts.views import UserLoginView, UserLogoutView, RegisterUserView
 
 app_name = 'accounts'
 urlpatterns = [
     path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
+    path('register/', RegisterUserView.as_view(), name='register'),
 ]
