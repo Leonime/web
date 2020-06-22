634fa03fce7a535432eb6eaa105214504767de18
Leopoldo Parra <lparra.dev@gmail.com>
2020-06-22 12:49:16 -0700

    and a url for the view

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from frontend.views import Index, ChirpDetail
+from frontend.views import ChirpDetail, ChirpList
 
 app_name = 'frontend'
 urlpatterns = [
-    path('frontend/', Index.as_view(), name='index'),
+    path('frontend/', ChirpList.as_view(), name='chirp_list'),
     path('frontend/<int:chirp_id>/', ChirpDetail.as_view(), name='chirp_detail'),
 ]
