fcad54b32552338e4cc4d57bb01ff199ce1dd992
Leopoldo Parra <lparra.dev@gmail.com>
2020-06-21 18:31:06 -0700

    Created a view for user registration form

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
-from django.contrib.auth import views
-from django.shortcuts import render
+from django.contrib.auth import views, login
+from django.shortcuts import render, redirect
+
+from accounts.forms import UserCreateForm
 
 
 class UserLoginView(views.LoginView):
@@ -17,3 +19,23 @@ class UserLoginView(views.LoginView):
 
 class UserLogoutView(views.LogoutView):
     template_name = 'account/auth.html'
+
+
+class RegisterUserView(views.FormView):
+    template_name = 'account/auth.html'
+    form_class = UserCreateForm
+    success_url = '/'
+
+    def post(self, request, *args, **kwargs):
+        super(RegisterUserView, self).get(request, *args, **kwargs)
+        form = self.form_class(request.POST or None)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(self.success_url)
+
+        context = {
+            "form": form,
+            "btn_label": "Register",
+        }
+        return render(request, self.template_name, context)
