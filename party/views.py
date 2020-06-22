29963327b01f3caf4791fea72f7ee2812bd6b684
Leopoldo Parra <lparra.dev@gmail.com>
2020-06-22 13:11:34 -0700

    Moved party serializers

--- party/views.py
+++ party/views.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 
 from party.models import Party, Wish, Photo
-from party.serializers import PartySerializer, WishSerializer, PhotoSerializer
+from party.api.serializers import PartySerializer, WishSerializer, PhotoSerializer
 
 
 class PartyViewSet(viewsets.ModelViewSet):
