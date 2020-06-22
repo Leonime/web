1d34d95638874ca131b22ff5a183f169a140bdce
Leopoldo Parra <lparra.dev@gmail.com>
2020-06-22 12:21:07 -0700

    added url for the template

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from frontend.views import Index
+from frontend.views import Index, ChirpDetail
 
 app_name = 'frontend'
 urlpatterns = [
     path('frontend/', Index.as_view(), name='index'),
-    path('frontend/<int:chirp_id>/', Index.as_view(), name='index'),
+    path('frontend/<int:chirp_id>/', ChirpDetail.as_view(), name='chirp_detail'),
 ]
