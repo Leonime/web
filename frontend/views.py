e23b085b573ba7fe5694160575024367a793b5dc
Leopoldo Parra <lparra.dev@gmail.com>
2020-06-22 12:20:38 -0700

    created a view for the chirp detail template.

--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,16 @@
+from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
 class Index(TemplateView):
     template_name = 'react/chirp/index.html'
+
+
+class ChirpDetail(TemplateView):
+    template_name = 'react/chirp/detail.html'
+
+    def get(self, request, *args, **kwargs):
+        super(ChirpDetail, self).get(request, *args, **kwargs)
+        chirp_id = self.kwargs.get('chirp_id' or None)
+        context = {"chirp_id": chirp_id}
+        return render(request, self.template_name, context)
