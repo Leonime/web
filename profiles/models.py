79830653ba6c71ea2d045e9841c000a22d028700
Leopoldo Parra <lparra.dev@gmail.com>
2020-06-23 06:50:39 -0700

    Added folower field, follower relation, and some descriptions

--- profiles/models.py
+++ profiles/models.py
@@ -1,9 +1,15 @@
 from django.contrib.auth.models import User
 from django.db import models
-from django.db.models.signals import post_save
+
+
+class FollowerRelation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, help_text='The user')
+    profile = models.ForeignKey("Profile", on_delete=models.CASCADE, help_text='The user profile')
+    timestamp = models.DateTimeField(auto_now_add=True, help_text='A record keeping timestamp.')
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    location = models.CharField(max_length=220, null=True, blank=True)
-    bio = models.TextField(blank=True, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, help_text='The user')
+    location = models.CharField(max_length=220, null=True, blank=True, help_text='The user location')
+    bio = models.TextField(blank=True, null=True, help_text='The user bio')
+    followers = models.ManyToManyField(User, related_name='following', blank=True, help_text='The user followers')
