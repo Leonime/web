019013b83e31ead5006c6d504758bc0c2f667976
Leopoldo Parra <lparra.dev@gmail.com>
2020-06-23 10:00:57 -0700

    Added a few fields to the user profile.

--- profiles/api/serializers.py
+++ profiles/api/serializers.py
@@ -4,15 +4,37 @@ from rest_framework import serializers
 from profiles.models import Profile
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['username']
-
-
 class ProfileSerializer(serializers.ModelSerializer):
-    user = UserSerializer()
+    first_name = serializers.SerializerMethodField(read_only=True)
+    last_name = serializers.SerializerMethodField(read_only=True)
+    username = serializers.SerializerMethodField(read_only=True)
+    follower_count = serializers.SerializerMethodField(read_only=True)
+    following_count = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Profile
-        fields = ['user', 'location', 'bio', 'followers']
+        fields = [
+            "first_name",
+            "last_name",
+            "id",
+            "bio",
+            "location",
+            "follower_count",
+            "following_count",
+            "username",
+        ]
+
+    def get_first_name(self, obj):
+        return obj.user.first_name
+
+    def get_last_name(self, obj):
+        return obj.user.last_name
+
+    def get_username(self, obj):
+        return obj.user.username
+
+    def get_following_count(self, obj):
+        return obj.user.following.count()
+
+    def get_follower_count(self, obj):
+        return obj.followers.count()
