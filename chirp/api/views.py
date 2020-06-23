fb472d675f16c627fd03488ef8323df22394fdbe
Leopoldo Parra <lparra.dev@gmail.com>
2020-06-23 10:07:27 -0700

    Overrided create method, now it will save the user.

--- chirp/api/views.py
+++ chirp/api/views.py
@@ -16,6 +16,19 @@ class ChirpViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly, ]
     pagination_class = RelativePageNumberPagination
 
+    def perform_create(self, serializer, *args, **kwargs):
+        if 'user' in kwargs:
+            serializer.save(user=kwargs['user'])
+        else:
+            serializer.save()
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer, user=request.user)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
     def list(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
 
