8aabc1fb3e431df53d8502e5707b3e06112b9f2c
Leopoldo Parra <lparra.dev@gmail.com>
2020-06-24 03:59:17 -0700

    Created a form to view the profile

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,9 +1,20 @@
 from django import forms
 
+from base.forms import ReadOnlyFormMixin
 from profiles.models import Profile
 
 
 class UserProfileForm(forms.ModelForm):
+    first_name = forms.CharField(required=False)
+    last_name = forms.CharField(required=False)
+    email = forms.CharField(required=False)
+
+    class Meta:
+        model = Profile
+        fields = ['location', 'bio']
+
+
+class UserProfileReadOnlyForm(ReadOnlyFormMixin):
     first_name = forms.CharField(required=False)
     last_name = forms.CharField(required=False)
     email = forms.CharField(required=False)
