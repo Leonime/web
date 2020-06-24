26c31ef1d9c49ded81d799f7c637ec2e1472cd83
Leopoldo Parra <lparra.dev@gmail.com>
2020-06-24 03:58:44 -0700

    Created a Mixin to create a read only form

---
+++ base/forms.py
@@ -0,0 +1,11 @@
+from django.forms import ModelForm
+
+
+class ReadOnlyFormMixin(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(ReadOnlyFormMixin, self).__init__(*args, **kwargs)
+        for key in self.fields.keys():
+            self.fields[key].widget.attrs['readonly'] = True
+
+    def save(self, *args, **kwargs):
+        pass
