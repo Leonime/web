f62f66e52895ecee729798f50b387d2fa798fdcf
Leopoldo Parra <lparra.dev@gmail.com>
2020-06-24 03:59:39 -0700

    A url for the view

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,11 +2,12 @@ from django.conf import settings
 from django.urls import path, include
 
 from profiles.api.routers import profiles_router
-from profiles.views import UserProfileView, UserProfileDetailView
+from profiles.views import UserProfileView, UserProfileDetailView, UserProfileReadOnlyView
 
 app_name = 'profiles'
 urlpatterns = [
     path('profiles/edit/', UserProfileView.as_view(), name='edit_profile'),
+    path('profiles/view/', UserProfileReadOnlyView.as_view(), name='view_profile'),
     path('profiles/<str:username>/', UserProfileDetailView.as_view(), name='user_profile'),
     path(f'{settings.API_URL}', include(profiles_router.urls)),
 ]
