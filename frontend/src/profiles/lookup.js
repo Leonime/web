7f548e22ba547ff769761bfafac6cb8b78afd9f5
Leopoldo Parra <lparra.dev@gmail.com>
2020-06-25 05:53:25 -0700

    Added api lookup for profile

--- frontend/src/profiles/lookup.js
+++ frontend/src/profiles/lookup.js
@@ -3,3 +3,8 @@ import {backendLookup} from "../lookup";
 export function apiProfileDetail(username, callback) {
     backendLookup("GET", `/api/v1/profiles/profile/profile/${username}/`, callback)
 }
+
+export function apiProfileFollowToggle(username, action, callback) {
+    const data = {action: `${action && action}`.toLowerCase()}
+    backendLookup("POST", `/api/v1/profiles/profile/follow/${username}/`, callback, data)
+}
