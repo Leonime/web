0ad1bf45dd2faf26fa3f1c192da106df1c26af6c
Leopoldo Parra <lparra.dev@gmail.com>
2020-06-25 09:40:29 -0700

    Created url for the template

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from frontend.views import ChirpDetail, ChirpList, ProfileDetail
+from frontend.views import ChirpDetail, ChirpList, ProfileDetail, ChirpFrontPageView
 
 app_name = 'frontend'
 urlpatterns = [
     path('frontend/', ChirpList.as_view(), name='chirp_list'),
+    path('frontend/front_page/', ChirpFrontPageView.as_view(), name='chirp_front_page'),
     path('frontend/<int:chirp_id>/', ChirpDetail.as_view(), name='chirp_detail'),
     path('frontend/profile/<str:username>/', ProfileDetail.as_view(), name='profile_detail'),
 ]
