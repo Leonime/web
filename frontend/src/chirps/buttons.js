971c57e99fb611a1cebf7e678718e72622a93862
Leopoldo Parra <lparra.dev@gmail.com>
2020-06-25 12:13:16 -0700

    Fixed like display'

--- frontend/src/chirps/buttons.js
+++ frontend/src/chirps/buttons.js
@@ -1,11 +1,12 @@
 import React from "react";
 import {apiChirpAction} from "./lookup";
+import numeral from "numeral";
 
 export function ActionBtn(props) {
     const {chirp, action, didPerformAction} = props
     const likes = chirp.likes ? chirp.likes : 0
     const className = props.className ? props.className : 'btn btn-primary btn-sm'
-    const actionDisplay = action.display ? action.display : 'Action'
+    const actionDisplay = action.type === 'chirp' ? likes > 1 ? action.display + 's' : action.display : action.display ? action.display : 'Action'
 
     const handleActionBackendEvent = (response, status) => {
         if ((status === 200 || status === 201) && didPerformAction) {
@@ -16,6 +17,6 @@ export function ActionBtn(props) {
         event.preventDefault()
         apiChirpAction(chirp.id, action.type, handleActionBackendEvent)
     }
-    const display = action.type === 'like' ? `${likes} ${actionDisplay}` : actionDisplay
+    const display = action.type === 'chirp' ? <span><span className="badge badge-dark">{numeral(likes).format("0 a")}</span> {actionDisplay}</span> : actionDisplay
     return <button className={className} onClick={handleClick}>{display}</button>
 }
