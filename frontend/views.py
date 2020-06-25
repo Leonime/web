1b4d2b223ac81fb30e83467ae2652a38c0f18013
Leopoldo Parra <lparra.dev@gmail.com>
2020-06-25 04:57:51 -0700

    created a view for the template

--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,10 @@
+from django.http import Http404
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from rest_framework.response import Response
+
+from profiles.api.serializers import ProfileSerializer
+from profiles.models import Profile
 
 
 class Index(TemplateView):
@@ -17,4 +22,25 @@ class ChirpDetail(TemplateView):
         super(ChirpDetail, self).get(request, *args, **kwargs)
         chirp_id = self.kwargs.get('chirp_id' or None)
         context = {"chirp_id": chirp_id}
+        return render(request, self.template_name, context)
+
+
+class ProfileDetail(TemplateView):
+    template_name = 'react/profiles/detail.html'
+
+    def get(self, request, *args, **kwargs):
+        username = kwargs.get('username' or None)
+        qs = Profile.objects.filter(user__username=username)
+        if not qs.exists():
+            raise Http404
+        profile_obj = qs.first()
+        is_following = False
+        if request.user.is_authenticated:
+            user = request.user
+            is_following = user in profile_obj.followers.all()
+        context = {
+            "username": username,
+            "profile": profile_obj,
+            "is_following": is_following
+        }
         return render(request, self.template_name, context)
