050b5691fc8e926ec15f78370f02d3e4132813d7
Leopoldo Parra <lparra.dev@gmail.com>
2020-06-26 11:46:32 -0700

    Fixed import

--- profiles/forms.py
+++ profiles/forms.py
@@ -4,7 +4,7 @@ from django import forms
 from django.conf import settings
 from django.utils.safestring import mark_safe
 
-from base.forms import ReadOnlyFormMixin
+from base.forms.forms import ReadOnlyFormMixin
 from profiles.models import Profile
 
 
