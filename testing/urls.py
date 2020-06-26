f7ff52216275758d92963079357e3f8a40291f54
Leopoldo Parra <lparra.dev@gmail.com>
2020-06-26 11:46:13 -0700

    A url for the template

--- testing/urls.py
+++ testing/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from testing.views import TestingList
+from testing.views import TestingList, TestProfileTemplate
 
 app_name = 'testing'
 
 urlpatterns = [
     path('', TestingList.as_view(), name='home'),
+    path('testing/profile', TestProfileTemplate.as_view(), name='test_profile')
 ]
