8b3032d8d45b3c25d6c947e231b853213009b390
Leopoldo Parra <lparra.dev@gmail.com>
2020-06-26 11:46:05 -0700

    Added a view for the template

--- testing/views.py
+++ testing/views.py
@@ -1,7 +1,11 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, TemplateView
 
 from testing.models import Testing
 
 
 class TestingList(ListView):
     model = Testing
+
+
+class TestProfileTemplate(TemplateView):
+    template_name = 'testing/profile.html'
