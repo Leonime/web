d18eda23b92082219576d00079ab6f9ce4d97eb5
Leopoldo Parra <lparra.dev@gmail.com>
2020-06-26 11:35:17 -0700

    The table is not used, and added image field to profile

--- profiles/models.py
+++ profiles/models.py
@@ -2,14 +2,10 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-class FollowerRelation(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, help_text='The user')
-    profile = models.ForeignKey("profiles.Profile", on_delete=models.CASCADE, help_text='The user profile')
-    timestamp = models.DateTimeField(auto_now_add=True, help_text='A record keeping timestamp.')
-
-
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, help_text='The user')
     location = models.CharField(max_length=220, null=True, blank=True, help_text='The user location')
     bio = models.TextField(blank=True, null=True, help_text='The user bio')
     followers = models.ManyToManyField(User, related_name='following', blank=True, help_text='The user followers')
+    image = models.ImageField(upload_to='pictures/profile/%Y/%m/%d/',
+                              max_length=512, blank=True, null=True, help_text='The profile picture')
