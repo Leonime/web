5781ba659c480de02b6408a77db4ac8ec77d6f32
Leopoldo Parra <lparra.dev@gmail.com>
2020-06-28 16:56:59 -0700

    Added caching to view.

--- shortener/views.py
+++ shortener/views.py
@@ -1,14 +1,21 @@
+from django.conf import settings
 from django.contrib.gis.geoip2 import GeoIP2
+from django.core.cache.backends.base import DEFAULT_TIMEOUT
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.utils.decorators import method_decorator
 from django.views import View
+from django.views.decorators.cache import cache_page
 from ipware import get_client_ip
 
 from analytics.models import SURLAnalytics
 from shortener.forms import SubmitUrlForm
 from shortener.models import ShortURL
 
+CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
 
+
+@method_decorator(cache_page(CACHE_TTL), name='dispatch')
 class ShortenerHome(View):
     form_class = SubmitUrlForm
     template_name = 'shortener/shortener_home.html'
