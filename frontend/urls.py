f6b3761712345c111a6c427fe91d33c2ec4c54fd
Leopoldo Parra <lparra.dev@gmail.com>
2020-06-27 22:14:54 -0700

    Added url to see user profile

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from frontend.views import ChirpDetail, ChirpList, ProfileDetail, ChirpFrontPageView
+from profiles.views import UserProfileDetailView
 
 app_name = 'frontend'
 urlpatterns = [
@@ -8,4 +9,5 @@ urlpatterns = [
     path('frontend/front_page/', ChirpFrontPageView.as_view(), name='chirp_front_page'),
     path('frontend/<int:chirp_id>/', ChirpDetail.as_view(), name='chirp_detail'),
     path('frontend/profile/<str:username>/', ProfileDetail.as_view(), name='profile_detail'),
+    path('frontend/<str:username>/profile/', UserProfileDetailView.as_view(), name='user_profile_detail'),
 ]
