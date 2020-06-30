e4d907467722a50a46f6193e8a9748634a9f20eb
Leopoldo Parra <lparra.dev@gmail.com>
2020-06-30 06:50:27 -0700

    Added django icons settings.

--- codeshepherds/settings/third_party_apps/__init__.py
+++ codeshepherds/settings/third_party_apps/__init__.py
@@ -2,5 +2,6 @@ from .celery import *
 from .channels import *
 from .cors import *
 from .django_crispy_forms import *
+from .django_icons import *
 from .django_rest_framework import *
 from .redis import *
