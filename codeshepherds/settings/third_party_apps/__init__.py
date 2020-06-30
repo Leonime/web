c34d36e2b5c10f08b0efa7f0fe5ae40fd149c586
Leopoldo Parra <lparra.dev@gmail.com>
2020-06-30 06:50:58 -0700

    Added dropbox settings.

--- codeshepherds/settings/third_party_apps/__init__.py
+++ codeshepherds/settings/third_party_apps/__init__.py
@@ -4,4 +4,5 @@ from .cors import *
 from .django_crispy_forms import *
 from .django_icons import *
 from .django_rest_framework import *
+from .dropbox import *
 from .redis import *
---
+++ codeshepherds/settings/third_party_apps/dropbox.py
@@ -0,0 +1,6 @@
+import os
+
+######################
+# Dropbox access key #
+######################
+DROPBOX_ACCESS_TOKEN = os.environ.get('DROPBOX_ACCESS_TOKEN')
