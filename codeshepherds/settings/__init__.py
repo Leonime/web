d8ceb9abe969ccb02ff62b9d4a6cdda12e9f0392
Leopoldo Parra <lparra.dev@gmail.com>
2020-06-30 06:25:54 -0700

    The global configurations required by local apps.

--- codeshepherds/settings/__init__.py
+++ codeshepherds/settings/__init__.py
@@ -1,5 +1,6 @@
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
+from .apps_custom_settings import *
 from .auth import *
 from .django import *
