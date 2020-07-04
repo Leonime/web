0b1224fecc84f9f74776a3f7aaaa7889459ec6c8
Leopoldo Parra <lparra.dev@gmail.com>
2020-07-03 22:44:49 -0700

    I don't want sentry running during debug

--- codeshepherds/settings/third_party_apps/sentry.py
+++ codeshepherds/settings/third_party_apps/sentry.py
@@ -5,10 +5,13 @@ from sentry_sdk.integrations.celery import CeleryIntegration
 from sentry_sdk.integrations.django import DjangoIntegration
 from sentry_sdk.integrations.redis import RedisIntegration
 
-##########
-# Sentry #
-##########
-sentry_sdk.init(
-    dsn=os.environ.get('SENTRY_DSN'),
-    integrations=[DjangoIntegration(), CeleryIntegration(), RedisIntegration()]
-)
+from codeshepherds.settings.django import DEBUG
+
+if not DEBUG:
+    ##########
+    # Sentry #
+    ##########
+    sentry_sdk.init(
+        dsn=os.environ.get('SENTRY_DSN'),
+        integrations=[DjangoIntegration(), CeleryIntegration(), RedisIntegration()]
+    )
