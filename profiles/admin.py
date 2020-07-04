329d7b5f9b57f06f8a1c9428113759c0c9675cbc
Leopoldo Parra <lparra.dev@gmail.com>
2020-07-04 03:26:46 -0700

    Changed to simple history model.

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
+from simple_history.admin import SimpleHistoryAdmin
 
 from profiles.models import Profile
 
 
-class ProfileAdmin(admin.ModelAdmin):
+class ProfileAdmin(SimpleHistoryAdmin):
     list_display = ['user', 'location', 'bio', 'image_tag']
     readonly_fields = ['image_tag']
 
