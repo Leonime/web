500a5da05d43559d828c079b1b9f36886454947a
Leopoldo Parra <lparra.dev@gmail.com>
2020-07-04 16:38:31 -0700

    Added django auth models to history.

--- history/models.py
+++ history/models.py
@@ -1,4 +1,6 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Permission, Group
 from simple_history import register
 
 register(User, app=__package__)
+register(Permission, app=__package__)
+register(Group, app=__package__)
