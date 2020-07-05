a44d0a4bbad43073fad50a181a98a0dcb3b78a58
Leopoldo Parra <lparra.dev@gmail.com>
2020-07-05 10:22:54 -0700

    Added custom layout for user form

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,5 +1,7 @@
 from string import Template
 
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Div, Row, HTML
 from django import forms
 from django.conf import settings
 from django.utils.safestring import mark_safe
@@ -10,7 +12,7 @@ from profiles.models import Profile
 
 class PictureWidget(forms.widgets.FileInput):
     def render(self, name, value, attrs=None, renderer=None):
-        html = Template("""<img class="cover" src="$media$link" width="128" height="128"/>""")
+        html = Template("""<img class="img_fill" src="$media$link"/>""")
         return mark_safe(html.substitute(media=settings.MEDIA_URL, link=value))
 
 
@@ -37,3 +39,36 @@ class UserProfileReadOnlyForm(ReadOnlyFormMixin):
     class Meta:
         model = Profile
         fields = ['location', 'bio', 'image']
+
+    def __init__(self, *args, **kwargs):
+        super(UserProfileReadOnlyForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.layout = Layout(
+            Row(
+                Div(
+                    Div(
+                        Div(
+                            HTML(
+                                """
+                                {% if form.image.value %}
+                                    <img class="img_fill" src="{{ form.image.value.url }}">
+                                {% endif %}
+                                """,
+                            ),
+                            css_class='my-2 mx-2'
+                        ),
+                        css_class='d-flex border border-rounded border-secondary'
+                    ),
+                    css_class='col-6'
+                ),
+                Div(
+                    'first_name',
+                    'last_name',
+                    'email',
+                    'bio',
+                    'location',
+                    css_class='col-6'
+                ),
+                css_class='col-10 mx-auto mt-2'
+            )
+        )
