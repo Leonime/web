c69032acd2440252fb324dbe5fb393b859fbea09
Leopoldo Parra <lparra.dev@gmail.com>
2020-07-09 04:48:28 -0700

    Added configuration for password reset time out

--- codeshepherds/settings/auth.py
+++ codeshepherds/settings/auth.py
@@ -1,6 +1,8 @@
 ######################
 # Auth urls defaults #
 ######################
+import os
+
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
@@ -23,3 +25,5 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+PASSWORD_RESET_TIMEOUT_DAYS = 2
