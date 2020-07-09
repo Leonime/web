593eb5816e58656308a7531cde2b7633823242be
Leopoldo Parra <lparra.dev@gmail.com>
2020-07-09 05:06:53 -0700

    Added confirm email url.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from accounts.views import UserLoginView, UserLogoutView, RegisterUserView
+from accounts.views import UserLoginView, UserLogoutView, RegisterUserView, ConfirmRegistrationView, \
+    ResendConfirmRegistrationView
 
 app_name = 'accounts'
 urlpatterns = [
     path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
     path('register/', RegisterUserView.as_view(), name='register'),
+    path('confirm-email/<str:user_id>/<str:token>/', ConfirmRegistrationView.as_view(), name='confirm_email'),
 ]
