516c7b557c593a1d1c4fa6ad11d984257b4d2099
Leopoldo Parra <lparra.dev@gmail.com>
2020-07-09 04:54:20 -0700

    Added type hint.

--- accounts/views.py
+++ accounts/views.py
@@ -28,7 +28,7 @@ class UserLoginView(views.LoginView):
     def post(self, request, *args, **kwargs):
         username = request.POST['username']
         password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
+        user: User = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             messages.success(request, 'You have successfully logged in.')
