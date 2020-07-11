9aeeb3e4bd82d6f4cbd3d28677ef42a23cc90c94
Leopoldo Parra <lparra.dev@gmail.com>
2020-07-11 07:27:36 -0700

    Added required urls.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from accounts.views import UserLoginView, UserLogoutView, RegisterUserView, ConfirmRegistrationView, \
-    ResendConfirmRegistrationView
+from accounts.views import (UserLoginView, UserLogoutView, RegisterUserView, ConfirmRegistrationView,
+                            ResendConfirmRegistrationView, UserPasswordResetView, UserPasswordResetConfirmView,
+                            UserPasswordResetDoneView)
 
 app_name = 'accounts'
 urlpatterns = [
@@ -10,5 +11,8 @@ urlpatterns = [
     path('register/', RegisterUserView.as_view(), name='register'),
     path('confirm-email/<str:user_id>/<str:token>/', ConfirmRegistrationView.as_view(), name='confirm_email'),
     path('resend-confirm-email/<str:user_id>/<str:token>/', ResendConfirmRegistrationView.as_view(),
-         name='resend-confirm_email'),
+         name='resend_confirm_email'),
+    path('password_reset/', UserPasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', UserPasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', UserPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 ]
