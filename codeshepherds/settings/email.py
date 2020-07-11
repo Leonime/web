adfba0090ca6d90522c3fd918ebeefb294171140
Leopoldo Parra <lparra.dev@gmail.com>
2020-07-11 07:27:56 -0700

    Added global setting for from email.

--- codeshepherds/settings/email.py
+++ codeshepherds/settings/email.py
@@ -8,3 +8,5 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_SEND_EMAIL = os.environ.get('EMAIL_SEND_EMAIL')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = os.environ.get('EMAIL_PORT')
+
+EMAIL_DEFAULT_FROM = 'webmaster@codeshepherds.com'
