7e979d72e56df4c911cadd961ed030d7097357ac
Leopoldo Parra <lparra.dev@gmail.com>
2020-07-14 11:20:49 -0700

    Created error 500 url

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from base.views import Error404
+from base.views import Error404, Error500
 
 app_name = 'chirp'
 urlpatterns = [
     path('404/', Error404.as_view(), name='404'),
+    path('500/', Error500.as_view(), name='500'),
 ]
